ccdbe485eaec915ee7c878bcce4c1a3016a83562
KingY33T <86491908+KingY33T@users.noreply.github.com>
2026-01-07 13:30:43 -0500

    testing scrolling on art page dialog

--- public/javascript/dialog.js
+++ public/javascript/dialog.js
@@ -1,18 +1,23 @@
 var dialog;
 var artwork;
+var inner;
 
 function selectdrawing(filename) {
   dialog = document.getElementById("dialog");
   artwork = document.getElementById(filename);
+  inner = document.getElementById("inner");
   dialog.style.display = "flex";
   artwork.style.display = "block";
+  inner.style.display = "block";
 }
 
 function closealldrawings() {
+  inner = document.getElementById("inner");
   dialog = document.getElementById("dialog");
   dialog.style.display = "none";
+  inner.style.display = "none";
   
-  let children = dialog.children;
+  let children = inner.children;
   for (let i = 0; i < children.length; i++) {
     children[i].style.display = "none";
   }
